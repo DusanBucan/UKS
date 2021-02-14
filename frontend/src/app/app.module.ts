a4347bbcd0b0e0b52277d0a2b9397c7833c98d54
sonjajos <sonjajosanov98@gmail.com>
2021-02-14 13:29:00 +0100

    Add login screen

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,36 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ServicesComponent } from './services/services.component';
-import { ProjectComponent } from './project/project.component';
-import { LabelComponent } from './label/label.component';
-import { UserComponent } from './user/user.component';
-import { MilestoneComponent } from './milestone/milestone.component';
-import { TaskComponent } from './task/task.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TeamComponent } from './team/team.component';
-import { CommitComponent } from './commit/commit.component';
-import { GithubUserComponent } from './github-user/github-user.component';
+import { AppComponent } from "./app.component";
+import { ProjectComponent } from "./project/project.component";
+import { LabelComponent } from "./label/label.component";
+import { MilestoneComponent } from "./milestone/milestone.component";
+import { TaskComponent } from "./task/task.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { TeamComponent } from "./team/team.component";
+import { CommitComponent } from "./commit/commit.component";
+import { GithubUserComponent } from "./github-user/github-user.component";
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServicesComponent,
     ProjectComponent,
     LabelComponent,
-    UserComponent,
     MilestoneComponent,
     TaskComponent,
     DashboardComponent,
     TeamComponent,
     CommitComponent,
-    GithubUserComponent
+    GithubUserComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
