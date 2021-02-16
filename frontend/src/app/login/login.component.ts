5c859577071568299b657d6623350b437fbb86d6
LeonaNedeljkovic <leona.nedeljkovic@gmail.com>
2021-02-16 16:24:30 +0100

    added frontend support for logging in and registration

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -25,7 +25,9 @@ export class LoginComponent implements OnInit {
       if (response !== null) {
         localStorage.setItem('currentUser', JSON.stringify({
           token: response['access']
+          
         }));
+        this.router.navigate(["/dashboard/home/issues"]);
         }
       }
     ,
