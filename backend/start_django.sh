634df0e8d0232283edd4815541678594e513fda4
DusanBucan <dusanbzr@gmail.com>
2021-02-16 17:11:06 +0100

    changed docker compose

--- backend/start_django.sh
+++ backend/start_django.sh
@@ -4,5 +4,5 @@
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-#python manage.py runserver 0.0.0.0:8000
-gunicorn helloapp.wsgi -b 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
+# gunicorn helloapp.wsgi -b 0.0.0.0:8000
