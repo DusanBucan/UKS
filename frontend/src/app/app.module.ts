a9efc0fb2603988a6f6fbc98ed6173c60137f7ee
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-02-17 22:16:00 +0100

    fix

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { IssuesComponent } from './issues/issues.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule,FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { TokenInterceptorService } from 'src/app/services/token-interceptor.service';
 import { IssueEditComponent } from './issue-edit/issue-edit.component';
 import { NewCommitComponent } from './commit/new-commit/new-commit.component';
