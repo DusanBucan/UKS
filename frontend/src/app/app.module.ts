9bfe2cd5d120d2e6c771f3a751ad857272f5b616
sonjajos <sonjajosanov98@gmail.com>
2021-02-18 00:49:15 +0100

    Add charts for issues on frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,29 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { ProjectComponent } from './project/project.component';
-import { LabelComponent } from './label/label.component';
-import { MilestoneComponent } from './milestone/milestone.component';
-import { TaskComponent } from './task/task.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TeamComponent } from './team/team.component';
-import { CommitComponent } from './commit/commit.component';
-import { GithubUserComponent } from './github-user/github-user.component';
-import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { IssuesComponent } from './issues/issues.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { HeaderComponent } from './header/header.component';
-import { NewMilestoneComponent } from './milestone/new-milestone/new-milestone.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule,FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { TokenInterceptorService } from 'src/app/services/token-interceptor.service';
-import { DetailsMilestoneComponent } from './milestone/details-milestone/details-milestone.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { ProjectComponent } from "./project/project.component";
+import { LabelComponent } from "./label/label.component";
+import { MilestoneComponent } from "./milestone/milestone.component";
+import { TaskComponent } from "./task/task.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { TeamComponent } from "./team/team.component";
+import { CommitComponent } from "./commit/commit.component";
+import { GithubUserComponent } from "./github-user/github-user.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { LoginComponent } from "./login/login.component";
+import { RegistrationComponent } from "./registration/registration.component";
+import { IssuesComponent } from "./issues/issues.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HeaderComponent } from "./header/header.component";
+import { NewMilestoneComponent } from "./milestone/new-milestone/new-milestone.component";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from "@angular/forms";
+import { TokenInterceptorService } from "src/app/services/token-interceptor.service";
+import { DetailsMilestoneComponent } from "./milestone/details-milestone/details-milestone.component";
 import { IssueEditComponent } from "./issue-edit/issue-edit.component";
 import { IssueCreateComponent } from "./issue-create/issue-create.component";
-import { WikiComponent } from './wiki/wiki.component';
-import { AngularEditorModule } from '@kolkov/angular-editor';
+import { WikiComponent } from "./wiki/wiki.component";
+import { AngularEditorModule } from "@kolkov/angular-editor";
+import { InsightsComponent } from "./insights/insights.component";
+import { NgApexchartsModule } from "ng-apexcharts";
 
 @NgModule({
   declarations: [
@@ -46,8 +55,17 @@ import { AngularEditorModule } from '@kolkov/angular-editor';
     NewMilestoneComponent,
     DetailsMilestoneComponent,
     WikiComponent,
+    InsightsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule,FormsModule, AngularEditorModule,ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    AngularEditorModule,
+    ReactiveFormsModule,
+    NgApexchartsModule,
+  ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
