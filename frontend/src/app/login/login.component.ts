9740fbeb5dbacd3ee129e2c6b5a5740c16b98065
DusanBucan <dusanbzr@gmail.com>
2021-02-18 14:47:58 +0100

    still testing..

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -27,7 +27,7 @@ export class LoginComponent implements OnInit {
           token: response['access']
           
         }));
-        console.log("radi automatski deployment preko jenkinsa 222")
+        console.log("radi automatski deployment preko jenkinsa 3")
         this.router.navigate(["/dashboard/home/issues"]);
         }
       }
