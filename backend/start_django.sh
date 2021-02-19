bcb72dec76816f7d3cadbde7c9f6d4b5a2c2b365
DusanBucan <dusanbzr@gmail.com>
2021-02-19 22:12:52 +0100

    first phase of test done

--- backend/start_django.sh
+++ backend/start_django.sh
@@ -14,6 +14,8 @@ python manage.py loaddata ./project/fixtures/project.json
 python manage.py loaddata ./milestone/fixtures/milestone.json
 python manage.py loaddata ./task/fixtures/task.json
 python manage.py loaddata ./team/fixtures/team.json
+python manage.py loaddata ./commit/fixtures/commit.json
+python manage.py loaddata ./wiki/fixtures/wiki.json
 
 # start django
 python manage.py runserver 0.0.0.0:8000
